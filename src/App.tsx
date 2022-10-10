1663158355f649638d56145eb83d71cc277be37a
icecrimu <charles.banigon@gmail.com>
2022-10-10 10:54:46 +0800

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
+import { About } from './pages/About';
+import { Home } from './pages/Home';
+import { Store } from './pages/Store';
 
 function App() {
-  return <h1 className="text-center">Hello World</h1>;
+  return (
+    <Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
