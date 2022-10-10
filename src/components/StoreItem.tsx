6638b509558039d8ad1f40ed06db1df5716b130d
icecrimu <charles.banigon@gmail.com>
2022-10-10 15:32:27 +0800

    Shopping Cart Context

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card, Button } from 'react-bootstrap';
+import { useShoppingCart } from '../context/ShoppingCartContext';
 import { formatCurrency } from '../utilities/formatCurrency';
 
 type StoreItemProps = {
@@ -9,7 +10,15 @@ type StoreItemProps = {
 };
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
-  const quantity = 0;
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart
+  } = useShoppingCart();
+
+  const quantity = getItemQuantity(id);
+
   return (
     <Card>
       <Card.Img
@@ -25,7 +34,9 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         </Card.Title>
         <div className="mt-auto">
           {quantity === 0 ? (
-            <Button className="w-100">+ Add To Cart</Button>
+            <Button onClick={() => increaseCartQuantity(id)} className="w-100">
+              + Add To Cart
+            </Button>
           ) : (
             <div
               className="d-flex align-items-center flex-column"
@@ -35,13 +46,17 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
                 className="d-flex align-items-center"
                 style={{ gap: '.5rem' }}
               >
-                <Button>-</Button>
+                <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
                 <div>
                   <span className="fs-3">{quantity}</span> in cart
                 </div>
-                <Button>+</Button>
+                <Button onClick={() => increaseCartQuantity(id)}>+</Button>
               </div>
-              <Button variant="danger" size="sm">
+              <Button
+                onClick={() => removeFromCart(id)}
+                variant="danger"
+                size="sm"
+              >
                 Remove
               </Button>
             </div>
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -9,7 +9,7 @@ export function Store() {
       <h1>Store</h1>
       <Row xs={1} md={2} lg={3} className="g-3">
         {storeItems.map(item => (
-          <Col>
+          <Col key={item.id}>
             <StoreItem {...item} />
           </Col>
         ))}
