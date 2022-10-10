dce2be55ee1adf215014d699738a9bd02d249b4a
icecrimu <charles.banigon@gmail.com>
2022-10-10 12:52:56 +0800

    add Store Page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,19 @@
+import { Row, Col } from 'react-bootstrap';
+import storeItems from '../data/items.json';
+
+import { StoreItem } from '../components/StoreItem';
+
 export function Store() {
-  return <div>Store</div>;
+  return (
+    <>
+      <h1>Store</h1>
+      <Row xs={1} md={2} lg={3} className="g-3">
+        {storeItems.map(item => (
+          <Col>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
